c50857b7ed68e2f6102250d2c9789b9ecaf04aab
Nick Cooper <nccooper00@gmail.com>
2022-01-24 10:52:52 -0800

    Fixed some issues with saving cities

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ var APIKey = "f199dda3b5bf9b230995121924fee94f" + "&units=imperial";
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-
-
-var weatherData;
-var favoriteData = {
+var cities = {
 
 };
+
+var weatherData;
 
 var nameDateEl = document.getElementsByClassName("cityName");
 var maintempEl = document.getElementsByClassName("maintemp");
@@ -17,17 +16,13 @@ var tempEl = document.getElementsByClassName("temp");
 var windEl = document.getElementsByClassName("wind");
 var humidityEl = document.getElementsByClassName("humidity");
 var uvindexEl = document.getElementsByClassName("uvindex");
+var cardImageEl = document.getElementsByClassName("cardImage");
+
+
+var saveBtnEl = document.getElementsByClassName("searchButton");
 
 
 
-
-
-var cityInput = document.getElementById('cityInput');
-document.querySelector('form.search').addEventListener('submit', (e) => {
-    e.preventDefault();
-
-
-});
 
 var city = cityInput.value;
 
@@ -81,6 +76,7 @@ function findCoords(latitude, longitude) {
             windEl[i].textContent = "Wind: " + weather.wind_speed + " MPH";
             humidityEl[i].textContent = "Humidity: " + weather.humidity;
             uvindexEl[0].textContent = "UV Index: " + weatherData.current.uvi;
+            cardImageEl[i].setAttribute("src", "http://openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png");
         }
     };
 
@@ -114,12 +110,74 @@ function findCoords(latitude, longitude) {
         return temp;
     }
 
+    
+    
+    var savedCityCollection = [];
+    
+    document.querySelector(".searchButton").addEventListener("click", (cities) => {
+        cities = document.getElementById('cityInput');
+        savedCityCollection.push(cities.value);
+        localStorage.setItem("cities", JSON.stringify(savedCityCollection));
+        console.log(cities.length);
 
-    function saveFavoriteData() {
-        localStorage.setItem("favoriteData", JSON.stringify(favoriteData));
-    }
+        // for (var i = 0; i < cities.length; i++) {
+        var ulEl = document.querySelector("ul");
+        var liEl = document.createElement('li');
+        ulEl.appendChild(liEl);
+
+        document.querySelector('li').setAttribute("onclick", "searchSaved()");
+        document.querySelector('li').setAttribute("id", "savedCityList");
+
+        const liNumber = document.querySelectorAll("li");
+
+
+        for (var i = 0; i < liNumber.length; i++)
+        document.querySelector("li").append(cities.value);
+
+
+        // let div = document.getElementById("savedCityList")
+        // div.append(cities[i]);
+        // };
+    });
 
     function loadFavoriteData() {
-        favoriteData = JSON.parse(localStorage.getItem("favoriteData"));
+        cities = JSON.parse(localStorage.getItem("cities"));
+    };
+
+
+    function searchSaved() {
+       const inputSpace = document.getElementById("cityInput");
+       const liCity = document.getElementById("savedCityList").textContent;
+       inputSpace.append(liCity);
     }
+
+
+    // saveBtnEl.addEventListener("click", function() {
+    //     var title = saveBtnEl.dataset.title;
+    //     console.log(title);
+    //         favoriteData[title] = { 
+    //             longitude: saveBtnEl.dataset.longitude,
+    //             latitude: saveBtnEl.dataset.latitude,
+    //             text: saveBtnEl.dataset.bodyText,
+    //             image: saveBtnEl.dataset.image
+    //         };
+    //     saveFavoriteData();
+    //     renderFavorite();
+    // });
+
+
+    // function renderFavorite() {
+    //     favoriteBoxEl.innerHTML = "";
+    //     for (var [key] of Object.entries(favoriteData)) {
+    //         searchButton.insertAdjacentHTML("beforeend", `<option>${key}</option>`);
+    //     }
+    // }
+
+    // function saveFavoriteData() {
+    //     localStorage.setItem("favoriteData", JSON.stringify(favoriteData));
+    // }
+
+    // function loadFavoriteData() {
+    //     favoriteData = JSON.parse(localStorage.getItem("favoriteData"));
+    // }
     
